eeff3757a297a5fe661f67799455440210647e37
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-02-01 15:14:44 +0200

    loading data using useLoaderData

--- app/routes/posts/index.jsx
+++ app/routes/posts/index.jsx
@@ -1,9 +1,38 @@
-import {useLoaderData} from  'remix';
+import {useLoaderData,Link} from  'remix';
+
+
+export const loader = ()=>{
+    const data ={
+        posts:[
+            {id:1, title:'post 1', body:'This is test post'},
+            {id:2, title:'post 2', body:'This is successfull post'},
+            {id:3, title:'post 3', body:'This is faile post'}
+        ]
+    }
+    return data;
+}
 
 function PostItem() {
-  return <div>
+
+    const {posts} = useLoaderData();
+  return <>
+      <div className="page-header">
       <h1>Posts</h1>
-  </div>;
+      <Link to='/posts/new' className='btn'>
+          New post
+      </Link>
+      </div>
+      <ul className="posts-list">
+      {posts.map(post=>(
+          <li key={post.id}>
+              <Link to={post.id}>
+                   <h3>{post.title}</h3>
+              </Link>
+          </li>
+      ))}
+      </ul>
+     
+  </>;
 }
 
 export default PostItem;
