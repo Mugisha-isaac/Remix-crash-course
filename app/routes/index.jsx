f0201a59cce6120d1b80354b88b65124c6f44ac4
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-02-01 14:28:47 +0200

    exporting meta tags to be resuable

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -2,6 +2,10 @@
        return(
          <div>
             <h1>welcome</h1>
+            <p>
+               Remix Is A Full Stack Framework Created By The Creators Of  React Routers.
+               This Is A Simple Blog From MUGISHA ISAAC
+            </p>
          </div>
        )
    }
