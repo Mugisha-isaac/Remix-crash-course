395683aa3c91bc1bb4370013f7657e671e71d30b
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-02-02 19:34:04 +0200

    configuration of db on both production and development lever

--- prisma/seed.js
+++ prisma/seed.js
@@ -5,7 +5,7 @@ const db = new PrismaClient();
 
 async function seed(){
     await Promise.all(
-        getPosts.map(post=>{
+        getPosts().map(post=>{
             return db.post.create({data:post});
         })
     )
