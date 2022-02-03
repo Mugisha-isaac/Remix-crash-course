63ffdd7b650be315d87f555bf1602a42cb199c0b
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-02-03 12:19:18 +0200

    writing query to database

--- app/routes/posts/index.jsx
+++ app/routes/posts/index.jsx
@@ -1,19 +1,24 @@
 import {useLoaderData,Link} from  'remix';
+import {db} from '~/utils/db.server';
 
-
-export const loader = ()=>{
+export const loader = async()=>{
     const data ={
-        posts:[
-            {id:1, title:'post 1', body:'This is test post'},
-            {id:2, title:'post 2', body:'This is successfull post'},
-            {id:3, title:'post 3', body:'This is faile post'}
-        ]
+        // posts: await db.post.findMany({
+        //   take:20,
+        //   select:{id:true,title:true,body:true},
+        //   orderBy:{createdAt:'desc'}
+        // })
+        posts: await db.post.findMany(
+            {
+                select:{id:true, title:true, createdAt:true},
+                orderBy:{createdAt:'desc'},
+                take:20
+            })
     }
     return data;
 }
 
 function PostItem() {
-
     const {posts} = useLoaderData();
   return <>
       <div className="page-header">
@@ -27,6 +32,7 @@ function PostItem() {
           <li key={post.id}>
               <Link to={post.id}>
                    <h3>{post.title}</h3>
+                   { new Date(post.createdAt).toLocaleDateString()}
               </Link>
           </li>
       ))}
