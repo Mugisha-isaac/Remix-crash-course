bb83f9bc4551c65c0fcf5bf4d20c358f97cf8df7
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-02-07 14:53:52 +0200

    Login is working

--- app/routes/auth/login.jsx
+++ app/routes/auth/login.jsx
@@ -1,6 +1,6 @@
 import { useActionData, redirect, json } from 'remix'
 import { db } from '~/utils/db.server'
-import { createUsersSession, Login } from '~/utils/session.server'
+import { createUserSession, login} from '~/utils/session.server'
 
 function validateUsername(username) {
   if (typeof username !== 'string' || username.length < 3) {
@@ -37,65 +37,142 @@ export const action = async ({ request }) => {
 
   switch (loginType) {
     case 'login': {
-      const user = await Login({ username, password })
-      console.log(user);
+      // Find user
+      const user = await login({ username, password })
+
+      // Check user
       if (!user) {
-        
         return badRequest({
           fields,
           fieldErrors: { username: 'Invalid credentials' },
         })
       }
-      return createUsersSession(user.id, '/posts')
+
+      // Create Session
+      return createUserSession(user.id, '/posts')
     }
-    case 'register':{
-    }
-    default:{
-      return badRequest(
-        {
+    case 'register': {
+      // Check if user exists
+      const userExists = await db.user.findFirst({
+        where: {
+          username,
+        },
+      })
+      if (userExists) {
+        return badRequest({
           fields,
-          formError:'Invalid Login Type'
-        }
-        )
+          fieldErrors: { username: `User ${username} already exists` },
+        })
+      }
+
+      // Create user
+      const user = await register({ username, password })
+      if (!user) {
+        return badRequest({
+          fields,
+          formError: 'Something went wrong',
+        })
+      }
+
+      // Create session
+      return createUserSession(user.id, '/posts')
+    }
+    default: {
+      return badRequest({
+        fields,
+        formError: 'Login type is invalid',
+      })
     }
   }
+
+  return redirect('/posts')
 }
 
-const login = () => {
-  const actionData = useActionData();
-  return <div className="auth-container">
-           <div className="page-header">
-           <h1>Login</h1>
-           </div>
-            <div className="page-content">
-            <form method="POST">
-              <fieldset>
-                <legend>Login or Register</legend>
-                <label htmlFor="">
-                  <input type="radio" name="loginType" value="loginType" defaultChecked={!actionData?.fields?.loginType || actionData?.fields?.loginType ==='login'} /> Login
-                </label>
-                <label htmlFor="">
-                  <input type="radio" name="Type" value="register"/> Register
-                </label>
-              </fieldset>
-              <div className="form-control">
-                <label htmlFor="username">Username</label>
-                <input type="text" name="username" id="username" defaultValue={actionData?.fields?.username} />
-                <div className="error">
-                  {actionData?.fieldErrors?.username && (actionData?.fieldErrors?.username)}
-                </div>
-              </div>
-              <div className="form-control">
-                <label htmlFor="password">Password</label>
-                <input type="password" name="password" id="password" defaultValue={actionData?.fields?.password} />
-                <div className="error">
-                  {actionData?.fieldErrors?.password && (actionData?.fieldErrors?.password)}
-                </div>
-              </div>
-              <button className="btn btn-black" type="submit">Submit</button>
-            </form>
-            </div>
-         </div>;
-};
+function Login() {
+  const actionData = useActionData()
 
-export default login;
+  return (
+    <div className='auth-container'>
+      <div className='page-header'>
+        <h1>Login</h1>
+      </div>
+
+      <div className='page-content'>
+        <form method='POST'>
+          <fieldset>
+            <legend>Login or Register</legend>
+            <label>
+              <input
+                type='radio'
+                name='loginType'
+                value='login'
+                defaultChecked={
+                  !actionData?.fields?.loginType ||
+                  actionData?.fields?.loginType === 'login'
+                }
+              />{' '}
+              Login
+            </label>
+
+            <label>
+              <input
+                type='radio'
+                name='loginType'
+                value='register'
+                defaultChecked={actionData?.fields?.loginType === 'register'}
+              />{' '}
+              Register
+            </label>
+          </fieldset>
+          <div className='form-control'>
+            <label htmlFor='username'>Username</label>
+            <input
+              type='text'
+              name='username'
+              id='username'
+              defaultValue={actionData?.fields?.username}
+            />
+            <div className='error'>
+              {actionData?.fieldErrors?.username ? (
+                <p
+                  className='form-validation-error'
+                  role='alert'
+                  id='username-error'
+                >
+                  {actionData.fieldErrors.username}
+                </p>
+              ) : null}
+            </div>
+          </div>
+
+          <div className='form-control'>
+            <label htmlFor='password'>Password</label>
+            <input
+              type='password'
+              name='password'
+              id='password'
+              defaultValue={actionData?.fields?.password}
+            />
+            <div className='error'>
+              {actionData?.fieldErrors?.password ? (
+                <p
+                  className='form-validation-error'
+                  role='alert'
+                  id='password-error'
+                >
+                  {actionData.fieldErrors.password}
+                </p>
+              ) : null}
+            </div>
+          </div>
+
+          <button className='btn btn-block' type='submit'>
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Login
--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -2,10 +2,8 @@ import bcrypt from 'bcrypt'
 import { db } from './db.server'
 import { createCookieSessionStorage, redirect } from 'remix'
 
-
 // Login user
-
-export async function Login({ username, password }) {
+export async function login({ username, password }) {
   const user = await db.user.findUnique({
     where: {
       username,
@@ -22,31 +20,74 @@ export async function Login({ username, password }) {
   return user
 }
 
-// const sessionSecret = process.env.SESSION_SECRET;
+// Register new user
+export async function register({ username, password }) {
+  const passwordHash = await bcrypt.hash(password, 10)
+  return db.user.create({
+    data: {
+      username,
+      passwordHash,
+    },
+  })
+}
 
-const sessionSecret ="secret";
+// Get session secret
+const sessionSecret = process.env.SESSION_SECRET
+if (!sessionSecret) {
+  throw new Error('No session secret')
+}
 
-if(!sessionSecret) throw new Error('No Session Secret');
-
+// Create session storage
 const storage = createCookieSessionStorage({
-  cookie:{
-    name:'remix_session',
+  cookie: {
+    name: 'remixblog_session',
     secure: process.env.NODE_ENV === 'production',
     secrets: [sessionSecret],
-    sameSite:'lax',
-    path:'/',
-    maxAge: 3600 * 24 * 60,
-    httpOnly:true
-  }
+    sameSite: 'lax',
+    path: '/',
+    maxAge: 60 * 60 * 24 * 60,
+    httpOnly: true,
+  },
 })
 
-
-export async function createUsersSession(userId: string, redirectTo: string) {
+// Create user session
+export async function createUserSession(userId: string, redirectTo: string) {
   const session = await storage.getSession()
   session.set('userId', userId)
   return redirect(redirectTo, {
     headers: {
       'Set-Cookie': await storage.commitSession(session),
+    },
+  })
+}
+
+// Get user session
+export function getUserSession(request: Request) {
+  return storage.getSession(request.headers.get('Cookie'))
+}
+
+// Get logged in user
+export async function getUser(request: Request) {
+  const session = await getUserSession(request)
+  const userId = session.get('userId')
+  if (!userId || typeof userId !== 'string') {
+    return null
+  }
+
+  try {
+    const user = await db.user.findUnique({ where: { id: userId } })
+    return user
+  } catch (error) {
+    return null
+  }
+}
+
+// Logout user and destroy session
+export async function logout(request: Request) {
+  const session = await storage.getSession(request.headers.get('Cookie'))
+  return redirect('/auth/logout', {
+    headers: {
+      'Set-Cookie': await storage.destroySession(session),
     },
   })
 }
