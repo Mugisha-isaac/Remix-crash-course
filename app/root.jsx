ae88a86442f5952e9ad79200affc96d1be0ce3b3
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-02-08 14:47:46 +0200

    assessing user info on front end

--- app/root.jsx
+++ app/root.jsx
@@ -1,5 +1,6 @@
-import {Outlet,LiveReload, Link, Links,Meta} from 'remix';
+import {Outlet,LiveReload, Link, Links,Meta,useLoaderData} from 'remix';
 import globalStylesUrl from '~/styles/global.css'
+import {getUser} from '~/utils/session.server'
 
 
 export const links = ()=>[{
@@ -15,6 +16,14 @@ export const meta = ()=>{
     desccription,
     keywords
   }
+}
+
+export const loader = async({request})=>{
+  const user = await getUser(request)
+  const data = {
+    user
+  }
+  return data;
 }
 
 export default function App(){
@@ -47,6 +56,7 @@ function Document({children, title}){
 
 
 function Layout({children}){
+  const {user} = useLoaderData();
     return(
       <>
        <nav className="navbar">
@@ -57,9 +67,15 @@ function Layout({children}){
            <li>
              <Link to='/posts' className='posts'>Posts</Link>
            </li>
-           <li>
+           {user ? (<li>
+             <form action="/auth/logout" method='POST'>
+               <button className='btn' type='submit'>
+                 Logout {user.username}
+               </button>
+             </form>
+           </li>): (<li>
              <Link to='/auth/login' className='posts'>Login</Link>
-           </li>
+           </li>)}
          </ul>
        </nav>
        <div className="container">
