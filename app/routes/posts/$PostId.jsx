72cc84824b1c6b9279c0a43f9d62245a1ac1d4e1
Mugisha-isaac <mugishaisaac2001@gmail>
2022-02-09 13:11:35 +0200

    authenticating user before deleting post

--- app/routes/posts/$PostId.jsx
+++ app/routes/posts/$PostId.jsx
@@ -1,32 +1,36 @@
 import {useLoaderData,Link,redirect} from 'remix'
 import {db} from '~/utils/db.server';
+import {getUser} from '~/utils/session.server';
 
-export const loader = async({params})=>{
+export const loader = async({params,request})=>{
+  const user = await getUser(request)
     const post  = await db.post.findUnique({
       where:{id:params.PostId}
     })
 
     if(!post) throw new Error('post not found');
-    const data = {post};
+    const data = {post,user};
     return data;
 }
 
 export const action = async ({ request, params }) => {
   const form = await request.formData()
   if (form.get('_method') === 'delete') {
+    const user = await getUser(request);
     const post = await db.post.findUnique({
       where: { id: params.PostId },
     })
 
     if (!post) throw new Error('Post not found')
+    if(user && post.userId === user._id){
       await db.post.delete({ where: { id: params.PostId } })
-
+    }
     return redirect('/posts')
   }
 }
 
 function Post() {
-  const {post} = useLoaderData();
+  const {post,user} = useLoaderData();
   return <>
     <div className="page-header">
     <h1>{post.title}</h1>
@@ -36,10 +40,11 @@ function Post() {
       {post.body}
     </div>
     <div className="page-footer">
-    <form method='POST'>
+    {user.id === post.userId && <form method='POST'>
             <input type='hidden' name='_method' value='delete' />
             <button className='btn btn-delete'>Delete</button>
-          </form>
+          </form>  }
+    
     </div>
   </>;
 }
